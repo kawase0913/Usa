02be67bf6d4c155c7141f532bf556241db9623f6
kawase0913 <21110019@oic-ok.ac.jp>
2022-12-09 16:34:21 +0900

    試験中

--- js/MAKEACCOUNT.js
+++ js/MAKEACCOUNT.js
@@ -1,2 +1,3 @@
-let NCMB = require("ncmb");
-let ncmb = new NCMB("","");
+let applicationKey = "d53f9a653bda166214deb80b3fe6825187dc78a5c26fb8b38469fe0436e8e2a5";
+let clientKey = "64f25d7b797210f4581015d44968b616dd2b619ee07fbad2293c0ef4e7716b7f";
+let ncmb = new NCMB(applicationKey,clientKey);
