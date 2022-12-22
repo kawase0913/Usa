f90530faa34b0769f03da4d50a62efd18089dff7
masashi1016 <21110047@oic-ok.ac.jp>
2022-12-22 11:53:49 +0900

    NORMALPOST.html,css更新

---
+++ js/NORMALPOST.js
@@ -0,0 +1,25 @@
+let applicationKey = "d53f9a653bda166214deb80b3fe6825187dc78a5c26fb8b38469fe0436e8e2a5";
+let clientKey = "64f25d7b797210f4581015d44968b616dd2b619ee07fbad2293c0ef4e7716b7f";
+let ncmb = new NCMB(applicationKey,clientKey);
+
+function throwNORMALPOST(){
+
+    let ArticleView = ncmb.DataStore("ArticleView");
+    let articleView = new ArticleView();
+
+
+    let Samne = document.getElementById("samne").value;
+    let Upfile = document.getElementById("upfile").value;
+    let ExplanatoryText = document.getElementById("explanatorytext").value;
+
+        articleView.set("title",Samne)
+        .set("picture",Upfile)
+        .set("sentence",ExplanatoryText)
+        .save()
+        .then(function(result){
+            console.log("投稿成功" + JSON.stringify(result));
+        })
+        .catch(function(error){
+            console.log("投稿失敗" + error + "," + JSON.stringify(error));
+        });
+}
